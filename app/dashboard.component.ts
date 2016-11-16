f84e8099eb612a0b5bc78ae2ed2beebf546088b9
sean78071 <sean.allen@exlservice.com>
2016-11-16 14:51:38 -0800

    Dashboard top heroes

--- app/dashboard.component.ts
+++ app/dashboard.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
+    moduleId: module.id,
     selector: 'my-dashboard',
-    template: `
-        <h3>My Dashboard</h3>
-    `
+    templateUrl: 'dashboard.entryComponents.html'
 })
 
 export class DashboardComponent { }

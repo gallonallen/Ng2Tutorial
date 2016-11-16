b49e4d6172b0e43f7841a6ec205bd221087394ea
sean78071 <sean.allen@exlservice.com>
2016-11-16 14:44:02 -0800

    Configure the dashboard route

--- app/app.module.ts
+++ app/app.module.ts
@@ -4,6 +4,7 @@ import { FormsModule }   from '@angular/forms';
 import { RouterModule }  from '@angular/router';
 
 import { AppComponent }        from './app.component';
+import { DashboardComponent }  from './dashboard.component';
 import { HeroDetailComponent } from './hero-detail.component';
 import { HeroesComponent }     from './heroes.component';
 import { HeroService }         from './hero.service';
@@ -14,6 +15,10 @@ import { HeroService }         from './hero.service';
     FormsModule,
     RouterModule.forRoot([
       {
+        path: 'dashboard',
+        component: DashboardComponent
+      },
+      {
         path: 'heroes',
         component: HeroesComponent
       }
@@ -21,6 +26,7 @@ import { HeroService }         from './hero.service';
   ],
   declarations: [ 
     AppComponent,
+    DashboardComponent,
     HeroDetailComponent,
     HeroesComponent
   ],

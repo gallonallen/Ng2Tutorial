e484aab596ff0252e73f1dc2bbebedce5e950421
sean78071 <sean.allen@exlservice.com>
2016-11-17 15:16:04 -0800

    HeroSearchComponent

--- app/app.module.ts
+++ app/app.module.ts
@@ -14,6 +14,7 @@ import { DashboardComponent }  from './dashboard.component';
 import { HeroDetailComponent } from './hero-detail.component';
 import { HeroesComponent }     from './heroes.component';
 import { HeroService }         from './hero.service';
+import './rxjs-extensions';
 
 @NgModule({
   imports: [ 

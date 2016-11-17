fa4daa9e403977730a10692c28edc1652ac375d1
sean78071 <sean.allen@exlservice.com>
2016-11-17 11:20:45 -0800

    Revise the HeroDetailComponent

--- app/hero-detail.component.ts
+++ app/hero-detail.component.ts
@@ -1,5 +1,10 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Params }   from '@angular/router';
+import { Location }                 from '@angular/common';
 
+import 'rxjs/add/operator/switchMap';
+
+import { HeroService } from './hero.service';
 import { Hero } from './hero';
 
 @Component({
@@ -16,7 +21,15 @@ import { Hero } from './hero';
     `
 })
 
-export class HeroDetailComponent {
+export class HeroDetailComponent implements OnInit {
   @Input()
   hero: Hero;
+
+  constructor(
+    private heroService: HeroService,
+    private route: ActivatedRoute,
+    private location: Location
+  ) { }
+
+  ngOnInit(): void { }
 }

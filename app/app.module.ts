306a8bda62157d7344ff61e3221aa4048d06d3ed
sean78071 <sean.allen@exlservice.com>
2016-11-17 15:20:02 -0800

    Add the search component to the dashboard

--- app/app.module.ts
+++ app/app.module.ts
@@ -14,6 +14,7 @@ import { DashboardComponent }  from './dashboard.component';
 import { HeroDetailComponent } from './hero-detail.component';
 import { HeroesComponent }     from './heroes.component';
 import { HeroService }         from './hero.service';
+import { HeroSearchComponent } from './hero-search.component';
 import './rxjs-extensions';
 
 @NgModule({
@@ -28,7 +29,8 @@ import './rxjs-extensions';
     AppComponent,
     DashboardComponent,
     HeroDetailComponent,
-    HeroesComponent
+    HeroesComponent,
+    HeroSearchComponent
   ],
   providers: [ HeroService ],
   bootstrap: [ AppComponent ]

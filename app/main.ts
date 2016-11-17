fb1d101835ad2150cb32811e6915498fd080d2d2
sean78071 <sean.allen@exlservice.com>
2016-11-17 15:32:41 -0800

    Dynamic bootstrapping with the JiT compiler.  This was the approach in the tutorial, so there are no changes to make if you followed the tutorial.

--- app/main.ts
+++ app/main.ts
@@ -1,4 +1,10 @@
+
+// The browser platform with a compiler
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+// The app module
 import { AppModule } from './app.module';
+
+// compile and launch the module
 const platform = platformBrowserDynamic();
 platform.bootstrapModule(AppModule);

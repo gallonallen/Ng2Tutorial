d63350922a42badbb6825b86d3d41dae7366e0ad
sean78071 <sean.allen@exlservice.com>
2016-11-17 14:18:40 -0800

    Error Handling

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -22,4 +22,9 @@ export class HeroService {
     return this.getHeroes()
                .then(heroes => heroes.find(hero => hero.id === id));
   }
+
+  private handleError(error: any): Promise<any> {
+    console.error('An error occurred', error); // for demo purposes only
+    return Promise.reject(error.message || error);
+  }
 }
